e2983a6d94688bbca7b7f18627a9c1199942041a
temirlan <k.temirlan.t@gmail.com>
2023-03-05 17:03:13 +0600

    Initial Commit

---
+++ HW-13-Kudaibergen_Temirlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-13-Kudaibergen_Temirlan
+//
+//  Created by Темирлан Кудайберген on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
